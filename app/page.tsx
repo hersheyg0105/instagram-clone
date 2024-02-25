330152af1d5709671a645704a667f71c2c021fe0
Harshit Gupta <hersheyg0105@gmail.com>
2024-02-25 16:47:03 -0600

    moved authOptions out of route.ts

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,15 @@ import GoogleButton from "react-google-button";
 import { signIn } from "next-auth/react";
 import { useSession } from "next-auth/react";
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+// import { authOptions } from "@/app/api/auth/[...nextauth]/options";
 import Modal from "./components/Modal";
 // import ModalDaisy from "./components/ModalDaisy";
 import ModalHeadless from "./components/ModalHeadless";
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
+  // const session = await getServerSession(authOptions);
   console.log("printing session in Home component");
-  console.log(session);
+  // console.log(session);
   return (
     <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
       {/* <h1>Hi There</h1>
