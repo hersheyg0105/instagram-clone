5bd7a232adec520370abd4a519cdf2b2a15ba9f2
Harshit Gupta <hersheyg0105@gmail.com>
2024-02-25 15:41:38 -0600

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-const authOptions = {
+export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
